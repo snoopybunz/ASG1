90691b5114fb43b46fe2c063ed7ba63af4256222
snoopybunz <s10241577@connect.np.edu.sg>
2023-12-01 00:11:50 +0800

    discovery update

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
 
+    if (n >= slides.length) {
+        slideIndex = 0; // Wrap around to the first slide
+    }
+
+    if (n < 0) {
+        slideIndex = slides.length - 1; // Go to the last slide
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex].style.display = "block";
+}
