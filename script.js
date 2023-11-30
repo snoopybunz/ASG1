0ca74b9c2b3b0ba32b7d1d9b54b60003eb92549f
snoopybunz <s10241577@connect.np.edu.sg>
2023-12-01 00:45:49 +0800

    update on discovery with javascript

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+let slideIndex = 0;
+function validateForm() {
+    var emailInput = document.getElementById('emailInput');
+    var emailValue = emailInput.value.trim();
+
+    if (emailValue === '') {
+        alert('Please enter a valid email address.');
+        return false; // Prevent form submission
+    }
+
+    // Display a confirmation message on the page
+    var confirmationMessage = document.getElementById('confirmationMessage');
+    confirmationMessage.innerHTML = 'Subscription successful!';
+
+    // Optionally, you can hide the form after successful submission
+    var form = document.querySelector('.newsletter form');
+    form.style.display = 'none';
+
+    return false; // Prevent form submission
+}
+
 function showSlides(n) {
     let i;
     const slides = document.getElementsByClassName("mySlides");
