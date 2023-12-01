2f5e0921e0129e851df4e885ddc8547160598e4f
snoopybunz <s10241577@connect.np.edu.sg>
2023-12-01 21:35:00 +0800

    update mobile view

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+// Global variable to keep track of the slide index
 let slideIndex = 0;
 
-//Function to validate the email input of the form//
+// Function to validate the email input in the form
 function validateForm() {
-    //get email input element//
+    // Get the email input element
     var emailInput = document.getElementById('emailInput');
-    //trim and get the value of the email input, trim is a method to remove any leading and trailing whitespaces from the string//
+    
+    // Trim and get the value of the email input
     var emailValue = emailInput.value.trim();
 
-    //Check if the email is empty//
+    // Check if the email is empty
     if (emailValue === '') {
         alert('Please enter a valid email address.');
         return false; // Prevent form submission
@@ -24,19 +26,31 @@ function validateForm() {
     return false; // Prevent form submission
 }
 
+// Slideshow functionality for rotating images
 let currentImageIndex = 0;
-        const images = document.querySelectorAll('.special-A-imgs img');
+const images = document.querySelectorAll('.special-A-imgs img');
 
-        function rotateImages() {
-            images[currentImageIndex].style.display = 'none';
+// Function to rotate images
+function rotateImages() {
+    images[currentImageIndex].style.display = 'none';
 
-            currentImageIndex = (currentImageIndex + 1) % images.length;
+    currentImageIndex = (currentImageIndex + 1) % images.length;
 
-            images[currentImageIndex].style.display = 'block';
-        }
+    images[currentImageIndex].style.display = 'block';
+}
 
-        // Initial call to start rotating images
-        rotateImages();
+// Initial call to start rotating images
+rotateImages();
 
-        // Set interval to rotate images every 2 seconds
-        setInterval(rotateImages, 2000);
+// Set interval to rotate images every 2 seconds
+setInterval(rotateImages, 2000);
+
+// Function to toggle display of navigation links on small screens
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
